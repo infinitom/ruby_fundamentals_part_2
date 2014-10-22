5ef176f20ff1bec354d9536f26de452722ea6324
infinitom <deva.nadesan@infinitom.com>
2014-10-22 15:01:21 -0400

    Setup user display and request user for formatted input

--- exercise5.rb
+++ exercise5.rb
@@ -2,3 +2,15 @@
 # Exercise 5 - A short program to convert imperial to metric (F to C)
 #
 
+system 'clear'
+
+puts "Temperature Converter\n---------------------"
+
+puts "Please specify the temperature and scale (e.g: 50C or 100F")
+user_input = gets.chomp
+
+
+
+
+
+
