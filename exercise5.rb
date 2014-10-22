4f2985ed08a6cf133e461a2f497efe2c8d61dc0f
infinitom <deva.nadesan@infinitom.com>
2014-10-22 15:34:19 -0400

    Working conversion calculator

--- exercise5.rb
+++ exercise5.rb
@@ -2,15 +2,36 @@
 # Exercise 5 - A short program to convert imperial to metric (F to C)
 #
 
+def temperature_converter(number, scale)
+# Thanks for awesome tips Josh
+
+	if scale == "F"
+		((number.to_i - 32) * 5 / 9).to_s + "C"
+	else
+		((number.to_i * 9 / 5) + 32).to_s + "F"
+	end
+end
+
+
 system 'clear'
 
 puts "Temperature Converter\n---------------------"
 
-puts "Please specify the temperature and scale (e.g: 50C or 100F")
+puts "Please specify the temperature and scale (e.g: 50C or 100F)"
 user_input = gets.chomp
 
 
+# Split the input string up into the number and scale,
+# last item will always be scale
 
+# Use slice - Thank you for the tip Leon
 
+# Returns the last piece of the input (F or C) to temp_scale and truncates
+# temperature to just numbers.
+temp_scale = user_input.slice! -1
 
+# Call conversion method
+user_output = temperature_converter(user_input, temp_scale)
 
+# Display the conversion
+puts "#{user_input}#{temp_scale} is #{user_output}"
