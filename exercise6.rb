82295e1771bb62692ba1af4c5309c3de41518b35
infinitom <deva.nadesan@infinitom.com>
2014-10-22 15:59:47 -0400

    Added a method to display the list

--- exercise6.rb
+++ exercise6.rb
@@ -2,10 +2,20 @@
 # Exercise 6 - A grocery list experiment
 #
 
+
+#Method to display the list
+def display_list(list)
+	
+	list.each do |list_item|
+		puts "* #{list_item}"
+	end
+
+end
+
+
+
 # Define grocery list array
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
 # Output grocery list
-grocery_list.each do |list_item|
-	puts "* #{list_item}"
-end
+display_list(grocery_list)
