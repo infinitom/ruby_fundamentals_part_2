258cbc3e7ab64eb36aa55e36e3020a3aa7fce42d
infinitom <deva.nadesan@infinitom.com>
2014-10-22 16:52:37 -0400

    Display original student list

--- exercise7.rb
+++ exercise7.rb
@@ -2,9 +2,21 @@
 # Exercise 7 - A grocery list experiment
 #
 
-# students
+
+#Method to display the list
+def display_list(list)
+	list.each do |key, value|
+		puts "#{key}: #{value} students"
+	end
+end
+
+
+# Students
 students = {
   :cohort1 => 34,
   :cohort2 => 42,
   :cohort3 => 22
 }
+
+# Display student list
+display_list(students)
