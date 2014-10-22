fd4fe1dc007d123115edd880952f8628c45b490b
infinitom <deva.nadesan@infinitom.com>
2014-10-22 15:54:18 -0400

    Define grocery list and output list for user

--- exercise6.rb
+++ exercise6.rb
@@ -2,4 +2,10 @@
 # Exercise 6 - A grocery list experiment
 #
 
+# Define grocery list array
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
+
+# Output grocery list
+grocery_list.each do |list_item|
+	puts "* #{list_item}"
+end
