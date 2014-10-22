4357b1227a8c1e2e44ffec5dfabf1e44e8f0de72
infinitom <deva.nadesan@infinitom.com>
2014-10-22 17:45:23 -0400

    Total number of students from all cohorts

--- exercise7.rb
+++ exercise7.rb
@@ -27,17 +27,21 @@ students[:cohort4] = 43
 
 
 # Display student list with new cohort
-puts "List with new cohort\n--------------------------"
+puts "List with new cohort\n--------------------"
 display_list(students)
 
 
 #Expanded classes by 5%
 puts "Expanded classes\n----------------"
-students.each { |key, value| students[key] = value*1.05}
+students.each { |key, value| students[key] = (value*1.05).to_i}
 display_list(students)
 
 
 #Deleted the second cohort
-puts "Deleted second cohort\n----------------"
+puts "Deleted second cohort\n----------------------"
 students.delete(:cohort2)
 display_list(students)
+
+#Bonus - Total number of students in cohort
+total_students = students.values.reduce :+
+puts "Total number of students: " + total_students.to_s
